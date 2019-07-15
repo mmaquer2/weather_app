fa5df517b6b75b090997c8e199add8e68430146d
mmaquer2 <mike.maquera212@gmail.com>
2019-07-14 20:34:31 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//1. fetch user zip code from markup
+
+//2. fetch api data from weather map
+
+
+
+function disp() {
+
+    var show = document.getElementById('results')
+    if(show.style.display = 'none') {
+        show.style.display = 'block'
+    }
+    else if ( show.style.display = 'block' ) {
+    
+        show.style.display ='none'
+    }
+    
+    //document.getElementById('results').style.display='block';
+    
+    }
+
+//3. change background color based on weather or current forecast
+    //if weather is sunny page = yellow sun animation 
+    //if weather is cold/snowwy = snowflakes animation
+    //
+
+
+//4.display 5 day weather and content on webpage
+
